c2e63c7364b6923509d755717c20f5f810a02a47
2284049 <70497897+2284049@users.noreply.github.com>
2020-10-20 22:47:34 -0700

    create pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./style/master.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Landing from "./components/pages/Landing";
+import SignIn from "./components/pages/SignIn";
+import Questionnaire from "./components/pages/Questionnaire";
+import Matches from "./components/pages/Matches";
+import Match from "./components/pages/Match";
+import NotFound from "./components/pages/NotFound";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-danger">Hello word</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/sign-in" component={SignIn} />
+        <Route exact path="/questionnaire" component={Questionnaire} />
+        <Route exact path="/matches" component={Matches} />
+        <Route exact path="/match" component={Match} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Questionnaire.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Questionnaire() {
+  return <h1>Questionnaire</h1>;
+}
