12cd082f7cca9ae267a12a510cd4656658637baa
2284049 <70497897+2284049@users.noreply.github.com>
2020-11-06 09:42:04 -0800

    change users flattened data schema

--- flattened/users.js
+++ flattened/users.js
@@ -1,10 +1,21 @@
+// TO DO:
+// ADD CURRENT USER & MATCH INFO UNDER USERS
+
 const users = [
    {
       id: "67b6d770-b0bf-45f7-8e82-5461eda8df7b",
       username: "walking_evergreen",
+      firstName: "",
+      lastName: "",
+      email: "",
+      phoneCountryCode: Number,
+      phoneAreaCode: Number,
+      phoneLineNumber: Number,
+      phoneExtension: Number,
       birthdate: "03241984",
       age: "36",
       score: 62,
+      password: "",
       createdAt: 1603342688819,
       verifyPhotoUrl:
          "https://static.wixstatic.com/media/7fa9fc_1b0170005fea4ea5b66760afedee15c4~mv2.jpg/v1/fill/w_1000,h_665,al_c,q_85/7fa9fc_1b0170005fea4ea5b66760afedee15c4~mv2.jpg",
